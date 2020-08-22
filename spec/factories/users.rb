7453295f47a3c406cee3e2433cab5bf793bb5559
daisuke0606 <68953503+daisuke0606@users.noreply.github.com>
2020-08-22 14:04:47 +0900

    user.passwordにFaker追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { 'abc123' }
+    password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     first_name            { '佐藤' }
     last_name             { '大佑' }

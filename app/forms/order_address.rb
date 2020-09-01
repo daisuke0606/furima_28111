b158bdafec5b7d4497127864baeba5849820eaf8
daisuke0606 <68953503+daisuke0606@users.noreply.github.com>
2020-09-01 15:56:23 +0900

    orderのvalidationの修正

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -2,12 +2,15 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefectures_id, :city, :block, :building, :phone, :order_id, :token
 
+  UNDER_BAR_EXSIT = /\A[0-9]{3}-[0-9]{4}\z/
+  NUMBER_DIFFERENT = /\A\d{10,11}\z/
+
   with_options presence: true do
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :post_code, format: { with: UNDER_BAR_EXSIT, message: 'is invalid. Include hyphen(-)' }
     validates :prefectures_id, numericality: { other_than: 0, message: 'を選択してください' }
     validates :city
     validates :block
-    validates :phone, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Include 10-11' }
+    validates :phone, format: { with: NUMBER_DIFFERENT, message: 'is invalid. Include 10-11' }
     validates :user_id
     validates :item_id
     validates :token
